a5beba7202a9ff3b1c27010926db2f26c514bbf4
CY, Lin <74100903+Lehend1010@users.noreply.github.com>
2021-08-09 05:23:15 +0800

    Add files via upload

---
+++ json_schema_generator/validator.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import json
+from jsonschema import validate
+
+
+class Validator(object):
+
+    def __init__(self, json_schema_dict):
+        self._json_schema_dict = json_schema_dict
+        self._error_message = ''
+
+    @property
+    def json_schema_dict(self):
+        return self._json_schema_dict
+
+    @property
+    def error_message(self):
+        return self._error_message
+
+    @classmethod
+    def from_path(self, path):
+        return Validator(json.load(open(path)))
+
+    def assert_json(self, json_str, is_load):
+
+        valid = False
+        if not is_load:
+            json_object = json.loads(json_str)
+        else:
+            json_object = json_str
+
+        try:
+            validate(json_object, self.json_schema_dict)
+            valid = True
+
+        except Exception as e:
+            self._error_message = "Inválido: \n\t%s" % str(e)
+
+        return valid
